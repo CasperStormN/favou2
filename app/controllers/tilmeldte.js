13982fdc04e6dcdaae12b4bb33f19704ee9d71d4
Thara Due Jeppesen <thar0076@edu.eal.dk>
2017-04-28 13:48:27 +0200

    deletebtn

--- app/controllers/tilmeldte.js
+++ app/controllers/tilmeldte.js
@@ -1,2 +1,29 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
+
+var buttonSwiped = 0;
+
+//$.deletebtn.show();
+
+function showdelete(){
+	$.deletebtn.hide();
+}
+
+/*
+function showDelete(){
+	if (buttonSwiped == 0)
+	{
+		$.deletinator.animate({
+			right: 0,
+			duration: 250,
+		});
+		buttonSwiped++;
+	} else {
+		$.deletinator.animate({
+			right: "-30%",
+			duration: 250,
+		});
+		buttonSwiped--;
+	}
+}
+*/
